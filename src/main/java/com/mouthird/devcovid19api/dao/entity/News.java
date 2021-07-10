3bc9d7716e531e46f1d5bc0c44aafb5d54d42aba
Ray <Ray_Xie@wistron.com>
2021-07-10 18:30:39 +0800

    [feat] unitest News model CRUD

--- src/main/java/com/mouthird/devcovid19api/dao/entity/News.java
+++ src/main/java/com/mouthird/devcovid19api/dao/entity/News.java
@@ -58,6 +58,14 @@ public class News {
         return id;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     @Override
     public String toString() {
         return "News{" +
--- src/test/java/com/mouthird/devcovid19api/NewsRepositoryIntegrationTests.java
+++ src/test/java/com/mouthird/devcovid19api/NewsRepositoryIntegrationTests.java
@@ -18,6 +18,7 @@ class NewsRepositoryIntegrationTests {
 
 	@BeforeEach
 	public void init() {
+		newsRepository.deleteAll();
 		for(int i=0; i<5; i++) {
 			News news = new News("test title", "7.12", "https://test.com",
 					"https://img.jpg", "This is test object.");
@@ -29,10 +30,29 @@ class NewsRepositoryIntegrationTests {
 	public void getAllNews() {
 		List<News> newsList = newsRepository.findAll();
 		assertEquals(5, newsList.size());
+	}
 
-		Long id = 1L;
-		for(News x : newsList)
-			assertEquals(id++, x.getId());
+	@Test
+	public void putNews() {
+		News news = newsRepository.findById(1L)
+				.orElseThrow(() -> new IllegalStateException(
+				"news with id 1L does not exists"
+		));
+		news.setTitle("Change Title");
+		newsRepository.save(news);
+		News modifiy_news = newsRepository.findById(1L)
+				.orElseThrow(() -> new IllegalStateException(
+						"news with id 1L does not exists"
+				));
+		assertEquals("Change Title", modifiy_news.getTitle());
+	}
+
+	@Test
+	public void deleteNews() {
+		List<News> newsList = newsRepository.findAll();
+		newsRepository.delete(newsList.get(0));
+		newsList = newsRepository.findAll();
+		assertEquals(4, newsList.size());
 	}
 
 }
