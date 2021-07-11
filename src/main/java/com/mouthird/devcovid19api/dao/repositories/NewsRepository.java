44f0cfa6e40fee086f23f3cd0416b1070c80a9aa
Ray <Ray_Xie@wistron.com>
2021-07-11 12:59:53 +0800

    [feat] add queryDSL

--- src/main/java/com/mouthird/devcovid19api/dao/repositories/NewsRepository.java
+++ src/main/java/com/mouthird/devcovid19api/dao/repositories/NewsRepository.java
@@ -2,8 +2,10 @@ package com.mouthird.devcovid19api.dao.repositories;
 
 import com.mouthird.devcovid19api.dao.entity.News;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NewsRepository extends JpaRepository<News, Long> {
+public interface NewsRepository extends
+        JpaRepository<News, Long>, QuerydslPredicateExecutor<News> {
 }
--- src/test/java/com/mouthird/devcovid19api/NewsRepositoryIntegrationTests.java
+++ src/test/java/com/mouthird/devcovid19api/NewsRepositoryIntegrationTests.java
@@ -1,13 +1,17 @@
 package com.mouthird.devcovid19api;
 
 import com.mouthird.devcovid19api.dao.entity.News;
+import com.mouthird.devcovid19api.dao.entity.QNews;
 import com.mouthird.devcovid19api.dao.repositories.NewsRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
@@ -16,14 +20,21 @@ class NewsRepositoryIntegrationTests {
 	@Autowired
 	private NewsRepository newsRepository;
 
+	@Autowired
+	private JPAQueryFactory queryFactory;
+
+	private QNews qNews = QNews.news;
+
 	@BeforeEach
 	public void init() {
 		newsRepository.deleteAll();
+		List<News> newsList = new ArrayList<>();
 		for(int i=0; i<5; i++) {
-			News news = new News("test title", "7.12", "https://test.com",
-					"https://img.jpg", "This is test object.");
-			newsRepository.save(news);
+			newsList.add(new News("test title", "7.12", "https://test.com",
+					"https://img.jpg", "This is test object."));
+
 		}
+		newsRepository.saveAll(newsList);
 	}
 
 	@Test
@@ -32,27 +43,21 @@ class NewsRepositoryIntegrationTests {
 		assertEquals(5, newsList.size());
 	}
 
+	@Transactional
 	@Test
 	public void putNews() {
-		News news = newsRepository.findById(1L)
-				.orElseThrow(() -> new IllegalStateException(
-				"news with id 1L does not exists"
-		));
-		news.setTitle("Change Title");
-		newsRepository.save(news);
-		News modifiy_news = newsRepository.findById(1L)
-				.orElseThrow(() -> new IllegalStateException(
-						"news with id 1L does not exists"
-				));
-		assertEquals("Change Title", modifiy_news.getTitle());
+		News news = queryFactory.selectFrom(qNews).fetchFirst();
+		Long status = queryFactory.update(qNews).where(qNews.id.eq(news.getId()))
+				.set(qNews.title, "Change Title").execute();
+		assertEquals(1, status);
 	}
 
+	@Transactional
 	@Test
 	public void deleteNews() {
-		List<News> newsList = newsRepository.findAll();
-		newsRepository.delete(newsList.get(0));
-		newsList = newsRepository.findAll();
-		assertEquals(4, newsList.size());
+		News news = queryFactory.selectFrom(qNews).fetchFirst();
+		Long status = queryFactory.delete(qNews).where(qNews.id.eq(news.getId())).execute();
+		assertEquals(1, status);
 	}
 
 }
