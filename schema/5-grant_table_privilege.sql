c0fbb6b814e8e20617f83c0bb42442aeadbc7050
Ray Xie <birken1018@gmail.com>
2021-11-23 09:38:02 +0000

    fix(db): readonly privilege

--- schema/5-grant_table_privilege.sql
+++ schema/5-grant_table_privilege.sql
@@ -4,6 +4,6 @@ GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON TABLE news to app_user;
 GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON TABLE videos to app_user;
 GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON TABLE channel to app_user;
 
-GRANT SELECT ON TABLE news to app_user;
-GRANT SELECT ON TABLE videos to app_user;
-GRANT SELECT ON TABLE channel to app_user;
+GRANT SELECT ON TABLE news to app_readonly;
+GRANT SELECT ON TABLE videos to app_readonly;
+GRANT SELECT ON TABLE channel to app_readonly;
