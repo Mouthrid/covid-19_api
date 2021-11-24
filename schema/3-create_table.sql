ac0601b8813ec4c8ebc8150f52cff05bc3123a22
Ray Xie <birken1018@gmail.com>
2021-11-24 08:38:27 +0000

    fix(db&docker) key and add extra_host

--- schema/3-create_table.sql
+++ schema/3-create_table.sql
@@ -1,11 +1,9 @@
 \connect covid19_db app_admin
 
 --the script to remove all tables in the database
-/*
 DROP TABLE IF EXISTS news CASCADE;
 DROP TABLE IF EXISTS video CASCADE;
 DROP TABLE IF EXISTS channel CASCADE;
-*/
 
 CREATE TABLE news (
   id varchar(1000) PRIMARY KEY not null,
@@ -21,6 +19,7 @@ CREATE TABLE video (
   id varchar(1000) PRIMARY KEY not null,
   title varchar(1000) not null,
   video_url varchar(1000) not null,
+  video_time timestamp with time zone not null default current_timestamp,
   img_url varchar(1000) not null,
   view_state varchar(1000) not null,
   duration varchar(1000) not null,
@@ -29,7 +28,7 @@ CREATE TABLE video (
 );
 
 CREATE TABLE channel (
-  id varchar(1000) PRIMARY KEY not null,
+  channel_id varchar(1000) PRIMARY KEY not null,
   name varchar(1000) not null,
   img_url varchar(1000) not null
 );
